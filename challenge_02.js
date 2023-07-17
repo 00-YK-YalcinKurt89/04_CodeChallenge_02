813bf8f829d3cb506c2515652d4bcf11d3aa9344
Yalcin <yalcin.coding@gmail.com>
2023-07-17 09:06:24 +0200

    added other versions of challenge

--- challenge_02.js
+++ challenge_02.js
@@ -17,6 +17,152 @@ Alter 13-17 // Cola
 /* Getränke-Challenge */
 
 
+
+
+
+
+/******** Variante I. Switch | Case ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// switch (true) {
+//     case (age >= 0 && age <= 5):
+//         console.log(`${firstName} trinkt Milch`);
+//         break;
+//     case (age >= 6 && age <= 12):
+//         console.log(`${firstName} trinkt Saft`);
+//         break;
+//     case (age >= 13 && age <= 17):
+//         console.log(`${firstName} trinkt Cola`);
+//         break;
+//     default:
+//         console.log(`${firstName} trinkt Wein`);
+//         break;
+// }
+
+
+
+
+
+
+/******** Variante Ia. Switch | Case - Kleinere Redundanz ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// let drink;
+
+// switch (true) {
+//     case (age >= 0 && age <= 5):
+//         drink = "Milch";
+//         break;
+//     case (age <= 12):
+//         drink = "Saft";
+//         break;
+//     case (age <= 17):
+//         drink = "Cola";
+//         break;
+//     default:
+//         drink = "Wein";
+//         break;
+// }
+
+// console.log(`${firstName} trinkt ${drink}`);
+
+
+
+
+
+
+/******** Variante II. IF / ELSE IF  ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// if (age >= 0 && age <= 5) {
+//     console.log(`${firstName} trinkt Milch`);
+// } else if (age >= 6 && age <= 12) {
+//     console.log(`${firstName} trinkt Saft`);
+// } else if (age >= 13 && age <= 17){
+//     console.log(`${firstName} trinkt Cola`);
+// } else {
+//     console.log(`${firstName} trinkt Wein`);
+// }
+
+
+
+
+
+
+/******** Variante IIa. IF / ELSE IF - Kleinere Redundanz ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// let drink;
+
+// if (age >= 0 && age <= 5) {
+//     drink = "Milch";
+// } else if (age <= 12) {
+//     drink = "Saft";
+// } else if (age <= 17){
+//     drink = "Cola";
+// } else {
+//     drink = "Wein";
+// }
+
+// console.log(`${firstName} trinkt ${drink}`);
+
+
+
+
+
+
+/******** Variante III. Ternärer Operator ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// let drink = (age >= 0 && age <= 5) ? "Milch" :
+//             (age <= 12) ? "Saft" :
+//             (age <= 17) ? "Cola" :
+//             "Wein";
+                                        
+// console.log(`${firstName} trinkt ${drink}`);
+
+
+
+
+
+
+/******** Variante Var_1 ********/
+
+// let firstName = prompt("Bitte Namen eingeben:");
+// let age = parseInt(prompt("Bitte Alter eingeben:"));
+
+// let drink;
+
+// if (!isNaN(age) && age >= 0) {
+//     if (age <= 5) {
+//         drink = "Milch";
+//     } else if (age <= 12) {
+//         drink = "Saft";
+//     } else if (age <= 17){
+//         drink = "Cola";
+//     } else {
+//         drink = "Wein";
+//     }
+//     console.log(`${firstName} trinkt ${drink}`);
+// } else {
+//     console.log("Tee");
+// }
+
+
+
+
+
+
 let userName, userAge;
 
 userName = prompt("Gib deinen Namen ein:");
